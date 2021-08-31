2a837dd08cdc373e3f3e9207925d33219aeab6f8
Daniel Perez <daniellukeperez@gmail.com>
2021-08-30 18:33:02 -0700

    adding comment

--- Tax.java
+++ Tax.java
@@ -4,7 +4,7 @@ public class Tax
    {
       for (int i=1; i<=6; i++) //adding a comment
       {
-         for (int j=1; j<=7-i; j++)
+         for (int j=1; j<=7-i; j++) //adding another comment
             System.out.print(j + " ");
          System.out.println();
       }
